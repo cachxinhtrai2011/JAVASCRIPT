15a9fdaa03ea7c07218bc0b94143406a6cd0a8eb
Thuan <ltt.9a1.dtthuan@gmail.com>
2019-12-16 18:56:42 +0700

    Fix

--- js/JS.js
+++ js/JS.js
@@ -1,41 +1,33 @@
-    var myBtn = document.getElementById("btn");
-   
-    myBtn.addEventListener("click", function () {
-            var myText = document.getElementById("txt").value;
+var myBtn = document.getElementById("Button");
+    
+myBtn.addEventListener("click", thisFunction);
+
+function thisFunction () {
+            var myText = document.getElementById("Text").value;
             var node = document.createElement("li");
-            node.innerHTML = myText + " " + "<button id='btnn'> Remove </button>" + "<button onclick = 'myFunction()' class='btnn2'>V</button>";
-            document.getElementById("cpl").appendChild(node);
-        
-            /*var textNode = document.createTextNode(myText);
-            node.appendChild(textNode);
-            document.getElementById("cpl").append(node);
+            let notCompleteList = document.getElementById("notComplete");
+            let completeList = document.getElementById("Complete");
+            let isComplete = false;
+            node.append(myText)
+            node.append(<button>Remove</button>)
+            node.append(<button>Move</button>);
+            notCompleteList.appendChild(node);
 
-            var nodeBtn = document.createElement("button");
-            nodeBtn.innerHTML = 'Xoa <button class="btn">X</button>';
-            document.getElementById("cpl").appendChild(nodeBtn);*/
-            node.addEventListener('click', function () {
-                this.parentNode.removeChild(this);
+
+            let btnRemove = node.children[1];
+            btnRemove.addEventListener('click', function (event) {
+                node.remove();
             });
-         })
-         function myFunction(){
-             alert("Chuyển xuống Not Completed Thành Công");
-             var myText2 = document.getElementById("txt").value;
-             var node2 = document.createElement("li");
-             node2.innerHTML = myText2 + " " + "<button id='btnn'> Remove </button>" + "<button onclick = 'myFunction2()' class='btnn2'>V</button>";
-             document.getElementById("ncpl").appendChild(node2);
 
-             node2.addEventListener('click', function () {
-                this.parentNode.removeChild(this);
+            let btnMove = node.children[2];
+            btnMove.addEventListener('click', function (event) {
+                if(isComplete){
+                  notCompleteList.append(node);
+                }
+                  else{
+                  completeList.append(node);
+                  }
+                  isComplete = !isComplete;
             });
          }
-         function myFunction2 (){
-             alert("Chuyển lên Completed Thành Công")
-            var myText3 = document.getElementById("txt").value;
-            var node3 = document.createElement("li");
-            node3.innerHTML = myText3 + " " + "<button id='btnn'> Remove </button>" + "<button onclick = 'myFunction()' class='btnn2'>V</button>";
-            document.getElementById("cpl").appendChild(node3);
-
-            node3.addEventListener('click', function () {
-                this.parentNode.removeChild(this);
-        })
-    }
+        
